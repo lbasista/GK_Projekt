7838ae2d4904c5f46b78238a5ffe87e1d5e31c5a
Artur Komenda <62992414+ArturK-85@users.noreply.github.com>
2025-04-27 14:12:46 +0200

    Update GK2025-MedianCut.cpp

--- GK2025-MedianCut.cpp
+++ GK2025-MedianCut.cpp
@@ -28,6 +28,28 @@ void paletaMedianCutBW(){
  SDL_UpdateWindowSurface(window);
 }
 
+void paletaMedianCutBW(){
+  ileKubelkow = 0;
+  ileKolorow = 0;
+  czyscPalete();
+  SDL_COLOR kolor;
+
+  int szary = 0;
+  int numer = 0;
+  int indeks = 0;
+
+  for(int y=0; y<wysokosc/2; y++){
+     for(int x=0; x<szerokosc/2; x++){
+        kolor = getPixel(x, y);
+        szary = 0.299*kolor.r + 0.587*kolor.g + 0.114*kolor.b;
+        obrazekS[numer] = szary;
+        setPixel(x + szerokosc/2, y, szary, szary, szary);
+        numer++;
+    }
+  }
+ SDL_UpdateWindowSurface(window);
+}
+
 void czyscPalete(){
   for(int k=0; k<ileKolorow; k++)
       paleta8[k] = {0, 0, 0);
