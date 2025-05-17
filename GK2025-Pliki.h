3f6560f78a71bd146f95612b4792f2a991b071da
Łukasz Basista <156651888+lbasista@users.noreply.github.com>
2025-05-17 12:54:34 +0200

    Update GK2025-Pliki.h

--- GK2025-Pliki.h
+++ GK2025-Pliki.h
@@ -1,36 +1,12 @@
 // funkcje do operacji na plikach
-#include "GK2025-Funkcje.h"
-#include "GK2025-Zmienne.h"
-#include "GK2025-Paleta.h"
-#include "GK2025-MedianCut.h"
-#include "GK2025-Pliki.h"
+#ifndef GK2025_PLIKI_H_INCLUDED
+#define GK2025_PLIKI_H_INCLUDED
 
-void odczytajPlik() {
-    SDL_Color kolor;
-    Uint16 szerokoscObrazka = 0;
-    Uint16 wysokoscObrazka = 0;
-    Uint8 ileBitow = 0;
-    char identyfikator[] = "  ";
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+#include <SDL2/SDL.h>
 
-    cout << "Odczytujemy plik 'obrazRGB.bin' uzywajac metody read()";
-    ifstream wejscie("obrazRGB.bin", ios::binary);
+#endif // GK2025_PLIKI_H_INCLUDED
 
-    wejscie.read((char*)&identyfikator, sizeof(char)*2);
-    wejscie.read((char*)&szerokoscObrazka, sizeof(Uint16));
-    wejscie.read((char*)&wysokoscObrazka, sizeof(Uint16));
-    wejscie.read((char*)&ileBitow, sizeof(Uint8));
-
-    cout << "id: " << identyfikator << endl;
-    cout << "szerokosc: " << wysokoscObrazka << endl;
-    cout << "wysokosc: " << szerokoscObrazka << endl;
-    cout << "ile bitow: " << ileBitow << endl;
-    
-    for (int y=0; y<wysokoscObrazka; y++) {
-        for (int x=0; x<szerokoscObrazka; x++) {
-            wejscie.read((char*)&kolor, sizeof(Uint8)*3);
-            setPixel(x+(szerokosc/2), y, kolor.r, kolor.g, kolor.b);
-        }
-    }
-
-    SDL_UpdateWindowSurface(window);
-}
+void odczytajPlik();
