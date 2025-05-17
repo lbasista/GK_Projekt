eaa8ae5d13c447b0bf34dea30b77b425cb8ea988
Łukasz Basista <156651888+lbasista@users.noreply.github.com>
2025-05-17 12:42:05 +0200

    U mnie działało
    To wy jesteście nieprzygotowani

--- GK2025-Pliki.cpp
+++ GK2025-Pliki.cpp
@@ -4,3 +4,27 @@
 #include "GK2025-Paleta.h"
 #include "GK2025-MedianCut.h"
 #include "GK2025-Pliki.h"
+
+
+void odczytajPlik() {
+    SDL_Color kolor;
+    Uint16 szerokoscObrazka = 0;
+    Uint16 wysokoscObrazka = 0;
+    Uint8 ileBitow = 0;
+    char identyfikator[] = "  ";
+
+    cout << "Odczytujemy plik 'obrazRGB.bin' uzywajac metody read()";
+    ifstream wejscie("obrazRGB.bin", ios::binary);
+
+    wejscie.read((char*)&identyfikator, sizeof(char)*2);
+    wejscie.read((char*)&szerokoscObrazka, sizeof(Uint16));
+    wejscie.read((char*)&wysokoscObrazka, sizeof(Uint16));
+    wejscie.read((char*)&ileBitow, sizeof(Uint8));
+
+    cout << "id: " << identyfikator << endl;
+    cout << "szerokosc: " << wysokoscObrazka << endl;
+    cout << "wysokosc: " << szerokoscObrazka << endl;
+    cout << "ile bitow: " << ileBitow << endl;
+
+    SDL_UpdateWindowSurface
+}
