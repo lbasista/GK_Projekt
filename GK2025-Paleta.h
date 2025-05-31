23c999331d60f469f9a9e54642334a21fb9b6dac
Artur Komenda <62992414+ArturK-85@users.noreply.github.com>
2025-05-31 13:39:50 +0200

    Update GK2025-Paleta.h

--- GK2025-Paleta.h
+++ GK2025-Paleta.h
@@ -1,4 +1,4 @@
-// funkcje do redukcji kolorów i tworzenia palet
+// funkcje do redukcji kolorÃ³w i tworzenia palet
 #ifndef GK2025_PALETA_H_INCLUDED
 #define GK2025_PALETA_H_INCLUDED
 #include <stdio.h>
@@ -11,5 +11,19 @@ using namespace std;
 void czyscPalete();
 void narysujPalete3b(int px, int py, SDL_Color pal3b[]);
 void narysujPalete(int px, int py, SDL_Color pal[]);
+
+void narzuconaV1();
+void narzuconaV2();
+void narzuconaV3();
+void narzuconaV4();
+void paletaNarzucona();
+void paletaWykryta();
+
+Uint8 z24Kdo5KS(SDL_Color kolor);
+SDL_Color z5KSdo24K(Uint8 szary5bit);
+void narzucona5KS();
+void narysujPalete5KS(int px, int py, int rozmiarBloku);
+
+void paletaMedianCutBW();
 
 #endif // GK2025_PALETA_H_INCLUDED
